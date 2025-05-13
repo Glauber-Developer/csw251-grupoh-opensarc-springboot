16d9f4d9115ea6af2554156db438612df680bf77
gabriellrmartins <gabriellrmartins@gmail.com>
2025-05-13 18:45:19 -0300

    ajustando pastas

--- src/main/java/com/sarc/sarc/infrastructure/ReservationRepository.java
+++ src/main/java/com/sarc/sarc/infrastructure/ReservationRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sarc.sarc.domain.entities.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation,Long>{
+    @Override
     Optional<Reservation> findById(Long id);
 }
--- src/main/java/com/sarc/sarc/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/sarc/sarc/infrastructure/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.sarc.sarc.config;
+package com.sarc.sarc.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
