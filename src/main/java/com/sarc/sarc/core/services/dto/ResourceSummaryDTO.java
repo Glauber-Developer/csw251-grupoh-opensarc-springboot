775f7d858c58a0dc1ccc0a1a004f0465b4ad2732
fxgvcv <gabriel.fogaca@edu.pucrs.br>
2025-05-13 20:05:08 -0300

    Comentando dto, porque não estão sendo utilizados ainda e estão quebrando.

--- src/main/java/com/sarc/sarc/core/services/dto/ResourceSummaryDTO.java
+++ src/main/java/com/sarc/sarc/core/services/dto/ResourceSummaryDTO.java
@@ -1,20 +1,20 @@
 package com.sarc.sarc.core.services.dto;
 
-import com.sarc.sarc.domain.enums.ResourceStatus;
-import com.sarc.sarc.domain.enums.ResourceType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+// import com.sarc.sarc.domain.enums.ResourceStatus;
+// import com.sarc.sarc.domain.enums.ResourceType;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
-/**
- * DTO com informações resumidas de um recurso
- */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class ResourceSummaryDTO {
-    private Long id;
-    private String name;
-    private ResourceType type;
-    private ResourceStatus status;
-}
+// /**
+//  * DTO com informações resumidas de um recurso
+//  */
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class ResourceSummaryDTO {
+//     private Long id;
+//     private String name;
+//     private ResourceType type;
+//     private ResourceStatus status;
+// }
